5d25bd799d277b0e9637951a653c897accb83f37
LeahTaurisano <leahtaurisano@gmail.com>
2024-01-10 22:03:46 -0800

    In Class Week 1
    Engine Prep Work

---
+++ WinterEngine/Framework/Core/Inc/Common.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+
+// win32 headers
+#include <objbase.h>
+#include <Windows.h>
+
+// std headers
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <cstdio>
+#include <cstdlib>
+#include <cstdint>
+#include <filesystem>
+#include <list>
+#include <map>
+#include <memory>
+#include <optional>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
