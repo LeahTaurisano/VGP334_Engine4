01e4e15b03d6bd863c5ff687a32de208fe1f357d
LeahTaurisano <leahtaurisano@gmail.com>
2024-01-31 21:59:24 -0800

    In Class Week 4 Pt2

--- WinterEngine/VGP242/02_HelloShape/GameState.h
+++ WinterEngine/VGP242/02_HelloShape/GameState.h
@@ -5,10 +5,39 @@
 class GameState : public WinterEngine::AppState
 {
 public:
-	void Initialize();
-	void Terminate();
+	void Initialize() override;
+	void Terminate() override;
+	void Update(float deltaTime) override;
+	void Render() override;
 
-	void Update(float deltaTime);
+protected:
+	virtual void CreateShape() {};
+	struct Vertex
+	{
+		WinterEngine::Math::Vector3 position;
+		WinterEngine::Color color;
+	};
 
-private:
+	using Vertices = std::vector<Vertex>;
+	Vertices mVertices;
+	ID3D11Buffer* mVertexBuffer = nullptr;
+	ID3D11VertexShader* mVertexShader = nullptr;
+	ID3D11InputLayout* mInputLayout = nullptr;
+	ID3D11PixelShader* mPixelShader = nullptr;
+};
+
+class TriangleState : public GameState
+{
+public:
+	void Update(float deltaTime) override;
+protected:
+	void CreateShape() override;
+};
+
+class SquareState : public GameState
+{
+public:
+	void Update(float deltaTime) override;
+protected:
+	void CreateShape() override;
 };
