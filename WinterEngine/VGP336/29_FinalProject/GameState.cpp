1049862cee95a50488736ca6e84247e3fbf1b0a7
LeahTaurisano <leahtaurisano@gmail.com>
2025-06-09 20:01:01 -0700

    Starting Setup for final

---
+++ WinterEngine/VGP336/29_FinalProject/GameState.cpp
@@ -0,0 +1,68 @@
+#include "GameState.h"
+#include "CustomDebugDrawService.h"
+#include "CustomDebugDrawComponent.h"
+
+using namespace WinterEngine;
+using namespace WinterEngine::Graphics;
+using namespace WinterEngine::Input;
+using namespace WinterEngine::Audio;
+using namespace WinterEngine::Physics;
+
+Service* CustomServiceMake(const std::string& serviceName, GameWorld& gameWorld)
+{
+	if (serviceName == "CustomDebugDrawService")
+	{
+		return gameWorld.AddService<CustomDebugDrawService>();
+	}
+	return nullptr;
+}
+
+Component* CustomComponentMake(const std::string& componentName, GameObject& gameObject)
+{
+	if (componentName == "CustomDebugDrawComponent")
+	{
+		return gameObject.AddComponent<CustomDebugDrawComponent>();
+	}
+	return nullptr;
+}
+
+Component* CustomComponentGet(const std::string& componentName, GameObject& gameObject)
+{
+	if (componentName == "CustomDebugDrawComponent")
+	{
+		return gameObject.GetComponent<CustomDebugDrawComponent>();
+	}
+	return nullptr;
+}
+
+void GameState::Initialize()
+{
+	// adds delegate callback to create a custom service
+	GameWorld::SetCustomService(CustomServiceMake);
+	GameObjectFactory::SetCustomMake(CustomComponentMake);
+	GameObjectFactory::SetCustomGet(CustomComponentGet);
+
+	mGameWorld.LoadLevel(L"../../Assets/Templates/Levels/test_level.json");
+}
+void GameState::Terminate()
+{
+	mGameWorld.Terminate();
+}
+
+void GameState::Update(float deltaTime)
+{
+	mGameWorld.Update(deltaTime);
+}
+
+void GameState::Render()
+{
+	mGameWorld.Render();
+	UIFont::Get()->DrawString(L"Hello World", Vector3::Zero, Colors::White);
+}
+
+void GameState::DebugUI()
+{
+	ImGui::Begin("Debug", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+	mGameWorld.DebugUI();
+	ImGui::End();
+}
